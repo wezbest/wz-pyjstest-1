53d8b8c9bba56fcec976953e45cc73aae3adcaa8
wezbest <110793713+wezbest@users.noreply.github.com>
2025-07-08 03:17:51 +0000

    🫦

---
+++ WX/uvinit.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+# This bash srcript is for installing the KL docker image here
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+
+hea1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+# UV Setup
+
+uvsetup() {
+    hea1 "UV Installation with packages"
+
+    # Get Name of project
+    echo -e "Enter the name of the project: "
+    read name_of_project
+    if [ -z "$name_of_project" ]; then
+        echo -e "${RED}BASTARD ! Project name cannot be empty${NC}"
+        exit 1
+    fi
+
+    # UC Commands
+    CO1="uv init $name_of_project"
+    CO2="cd $name_of_project"
+
+    DEPS="rich python-dotenv OpenAI"
+    CO3="uv add  $DEPS"
+    CO4="uv tree"
+
+    ## RUN Above Commands
+    echo -e "--- Executing ${CO1} ---"
+    eval "$CO1"
+    echo -e "--- Executing ${CO2} ---"
+    eval "$CO2"
+    echo -e "--- Executing ${CO3} ---"
+    eval "$CO3"
+    echo -e "--- Executing ${CO4} ---"
+    eval "$CO4"
+    echo -e "${GREEN}***** Installation Completed *****${NC}"
+}
+
+# Run
+uvsetup
