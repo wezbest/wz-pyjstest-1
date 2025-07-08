c21179fa85fbe8675a935c7b1ae91ea4abd1e2cc
wezbest <110793713+wezbest@users.noreply.github.com>
2025-07-08 03:28:07 +0000

    🫦

--- WX/nov/no1/src/n1.py
+++ WX/nov/no1/src/n1.py
@@ -1,3 +1,15 @@
 # ///////////////////////////////
 # n1.pu = Testing out models
 # ///////////////////////////////
+
+# --- Imports ----
+
+# --- Vars ----
+
+load_dotenv("src/.azz")
+NV_T = os.getenv("GHB")
+
+# --- Main ----
+
+
+# --- Subs ----
