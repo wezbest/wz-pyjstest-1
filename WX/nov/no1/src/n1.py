2c82bc5ad920578ec49c4a62fa5eafd02e55eb27
wezbest <110793713+wezbest@users.noreply.github.com>
2025-07-08 03:31:30 +0000

    🫦

--- WX/nov/no1/src/n1.py
+++ WX/nov/no1/src/n1.py
@@ -9,6 +9,8 @@ import os
 from dotenv import load_dotenv
 from rich import print as rpr
 
+from .utz import he1
+
 # --- Vars ----
 
 load_dotenv("src/.azz")
@@ -23,4 +25,5 @@ def n1_main():
 
 # --- Subs ----
 def brint_env():
+    he1("Brintaz Envaz")
     rpr(f"[bold green]NOV:[/bold green] {NV_T}")
