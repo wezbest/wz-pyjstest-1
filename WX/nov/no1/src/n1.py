6b2fa311cf6c087da231fca3e05c4f1709ce5620
wezbest <110793713+wezbest@users.noreply.github.com>
2025-07-08 03:29:48 +0000

    🫦

--- WX/nov/no1/src/n1.py
+++ WX/nov/no1/src/n1.py
@@ -4,12 +4,23 @@
 
 # --- Imports ----
 
+import os
+
+from dotenv import load_dotenv
+from rich import print as rpr
+
 # --- Vars ----
 
 load_dotenv("src/.azz")
-NV_T = os.getenv("GHB")
+NV_T = os.getenv("NOV")
 
 # --- Main ----
 
 
+def n1_main():
+    pass
+
+
 # --- Subs ----
+def brint_env():
+    rpr(f"[bold green]NOV:[/bold green] {NV_T}")
